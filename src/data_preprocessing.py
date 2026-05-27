f051aad0c1a68becae0fc552cfb9818f9288ad8c
Nacho <i.gomezo@duocuc.cl>
2026-05-27 16:53:21 -0400

    Agregando modelos

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,12 +1,13 @@
 import pandas as pd
 from pathlib import Path
+from sklearn.model_selection import train_test_split
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 
-def load_and_preprocess_data():
+def load_and_preprocess_data(test_size=0.2, random_state=42):
     """
-    Carga el dataset crudo, aplica limpieza de nulos ocultos y 
-    devuelve la matriz procesada (X) y el vector objetivo (y).
+    Carga el dataset crudo, aplica limpieza, divide en Train/Test y 
+    aplica las transformaciones evitando el Data Leakage.
     """
     # 1. Rutas dinámicas
     BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,9 +30,12 @@ def load_and_preprocess_data():
     X = df.drop(columns=['Churn'])
     y = df['Churn']
     
-    # 3. Pipeline de Preprocesamiento (Scikit-Learn)
+    # 3. DIVISIÓN TRAIN/TEST (¡El paso clave del profesor!)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
+    
+    # 4. Pipeline de Preprocesamiento (Scikit-Learn)
     numeric_features = ['tenure', 'MonthlyCharges', 'TotalCharges']
-    categorical_features = X.select_dtypes(include=['object']).columns.tolist()
+    categorical_features = X_train.select_dtypes(include=['object']).columns.tolist()
     
     preprocessor = ColumnTransformer(
         transformers=[
@@ -41,14 +45,21 @@ def load_and_preprocess_data():
         remainder='passthrough'
     )
     
-    # Transformación final y reconstrucción del DataFrame
-    X_processed = preprocessor.fit_transform(X)
-    feature_names = [n.split('__')[-1] for n in preprocessor.get_feature_names_out()]
-    X_df = pd.DataFrame(X_processed, columns=feature_names)
+    # 5. Transformación Diferenciada para evitar Data Leakage
+    X_train_processed = preprocessor.fit_transform(X_train)
+    X_test_processed = preprocessor.transform(X_test)
     
-    return X_df, y
+    # Reconstrucción de los DataFrames
+    feature_names = [n.split('__')[-1] for n in preprocessor.get_feature_names_out()]
+    
+    X_train_df = pd.DataFrame(X_train_processed, columns=feature_names)
+    X_test_df = pd.DataFrame(X_test_processed, columns=feature_names)
+    
+    return X_train_df, X_test_df, y_train, y_test
 
 # Bloque de prueba local
 if __name__ == "__main__":
-    X, y = load_and_preprocess_data()
-    print(f"Dataset procesado con éxito. Dimensiones de X: {X.shape}")
+    X_train, X_test, y_train, y_test = load_and_preprocess_data()
+    print(f"Dataset procesado con éxito sin Data Leakage.")
+    print(f"Dimensiones de X_train: {X_train.shape}")
+    print(f"Dimensiones de X_test: {X_test.shape}")
